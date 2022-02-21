a410a5a68414600b9b85f1484c677b5a4679a845
azurraaaa <60769725+azurraaaa@users.noreply.github.com>
2022-02-21 09:40:08 -0500

    Update spin_to_spin.py

--- Spin_To_Spin_Model/spin_to_spin.py
+++ Spin_To_Spin_Model/spin_to_spin.py
@@ -14,57 +14,56 @@ plt.rcParams['axes.titlesize'] = 20
 plt.rcParams['xtick.labelsize'] = 14
 plt.rcParams['ytick.labelsize'] = 14
 
-spin_state_total=np.loadtxt('a.txt',dtype=int)
+spin_state_total=np.loadtxt('spin_state_0.001000_different_init.txt',dtype=int)
 print(np.shape(spin_state_total))
 
 L = 100
 ave = L                         #average number of combinations
 J = 1
 kB = 1
-Ts = np.linspace( 0.0035, .005, 30 ) * J / kB
+Ts = np.linspace( 0.001, .004, 30 ) * J / kB
 n_record=1000
 n_total = 200000
 
 MI_total = []
 H_marginal_total = []
 H_joint_total = []
-Dist = range(1,100,5)
+Dist = range(1,100,10)
+Dist_len = len(Dist)
 
 #Calculate MI under the different temperatures and different spin-to-spin distance
 for num in range( len( Ts ) ):  # these are the temperatures
-    count = np.zeros((2000, 4))  # record the count
+    count = np.zeros((Dist_len*L*2, 4))  # record the count
     probability_joint = [0, 0, 0, 0]  # record the joint probability
     probability = [0, 0, 0, 0]  # record the probability
     probability_joint_show = np.zeros((2, 2))
-    MI = np.zeros((20))  # mutual information
-    H_marginal = np.zeros((20))
-    H_joint = np.zeros((20))
+    MI = np.zeros((Dist_len))  # mutual information
+    H_marginal = np.zeros((Dist_len))
+    H_joint = np.zeros((Dist_len))
 
     #count the two spin states among monte carlo process
     for step in range(n_record):
-        s = spin_state_total[num*n_total+step*200,:]
+        s = spin_state_total[(num)*n_record+step,:]
 
         #count spin states under different combinations and also different spin-to-spin distance
         for ave_num in range(ave):
-            for r2 in Dist:
-                distance_num = int((r2-1)/5)
-                loop = int(100 * distance_num)
+            for distance_index, r2 in enumerate(Dist):
+                loop = int(100 * distance_index)
                 r = ave_num
 
-                if (s[r] == 1 and s[r + r2 if r < L - r2 else r + r2 - L] == 1):
+                if (s[r] == 1 and s[(r+r2) % L] == 1):
                     count[ave_num+loop, 0] += 1
-                elif (s[r] == 1 and s[r + r2 if r < L - r2 else r + r2 - L] == -1):
+                elif (s[r] == 1 and s[(r+r2) % L] == -1):
                     count[ave_num+loop, 1] += 1
-                elif (s[r] == -1 and s[r + r2 if r < L - r2 else r + r2 - L] == 1):
+                elif (s[r] == -1 and s[(r+r2) % L] == 1):
                     count[ave_num+loop, 2] += 1
-                elif (s[r] == -1 and s[r + r2 if r < L - r2 else r + r2 - L] == -1):
+                elif (s[r] == -1 and s[(r+r2) % L] == -1):
                     count[ave_num+loop, 3] += 1
 
     #Cauculate the MI
     for ave_num in range(ave):
-        for r2 in Dist:
-            distance_num = int((r2 - 1) / 5)
-            loop = int(100 * distance_num)
+        for distance_index, r2 in enumerate(Dist):
+            loop = int(100 * distance_index)
             for i in range(2):
                 for k in range(2):
                     probability_joint_show[i, k] = count[ave_num+loop, i * 2 + k] / n_record
@@ -77,9 +76,9 @@ for num in range( len( Ts ) ):  # these are the temperatures
                 ave_num+loop, 2]) != 0 else 1
             probability[3] = (count[ave_num+loop, 1] + count[ave_num+loop, 3]) / n_record if (count[ave_num+loop, 1] + count[
                 ave_num+loop, 3]) != 0 else 1
-            MI[distance_num] += -sum(probability * np.log2(probability)) + sum(probability_joint * np.log2(probability_joint))
-            H_marginal[distance_num] += -sum(probability * np.log2(probability))
-            H_joint[distance_num] += -sum(probability_joint * np.log2(probability_joint))
+            MI[distance_index] += -sum(probability * np.log2(probability)) + sum(probability_joint * np.log2(probability_joint))
+            H_marginal[distance_index] += -sum(probability * np.log2(probability))
+            H_joint[distance_index] += -sum(probability_joint * np.log2(probability_joint))
     H_marginal = H_marginal / ave
     H_joint = H_joint / ave
     MI = MI / ave
@@ -118,12 +117,11 @@ for num in range( len( Ts ) ):
     plt.savefig('./H_joint_total%d.jpg'% (Ts[num]))
     plt.close()
     # plt.show()
-
 #plot the parameter as a function of temperature(under different distance)
 MI_total = np.array(MI_total).T
 H_marginal_total = np.array(H_marginal_total).T
 H_joint_total=np.array(H_joint_total).T
-for distance in range(20):
+for distance in range(Dist_len):
     plt.plot(Ts, MI_total[distance][:])
     plt.xlabel('Temperature')
     plt.ylabel('Mutual information')
@@ -142,4 +140,3 @@ for distance in range(20):
     plt.savefig('./H_joint_total%d.jpg'%(distance*5))
     plt.close()
     #plt.show()
-
